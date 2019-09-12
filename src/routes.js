71ccf45a8dc603c4d50e462ec08e19788d244eec
JefferssonSemin <jeffesemin@hotmail.com>
2019-09-12 15:48:57 -0300

    Adicionado o prettier em conjunto com o eslint usando os padrões da airbnb junto com o editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>{
-  return res.json({message: 'alo'});
-})
+routes.get('/', (req, res) => res.json({ message: 'alo' }));
 
 export default routes;
